755b25a4f030a03c6384def4ec8952a4af35b4a0
kiitoss <antoinechataignier1998@orange.fr>
2020-04-22 15:27:58 +0200

    feat: add 'tri comptage'

--- tris_de_listes.ml
+++ tris_de_listes.ml
@@ -16,10 +16,9 @@ liste aleatoire nb_entiers borne_max renvoie une liste de nombres entiers tirés
   -> La liste renvoyée peut bien sûr contenir des doublons.
 *)
 let rec liste_aleatoire nb_entiers borne_max =
-  if nb_entiers = 0 then
-    []
-  else
-    Random.int borne_max :: liste_aleatoire (nb_entiers-1) borne_max;;
+  if nb_entiers = 0
+  then []
+  else Random.int borne_max :: liste_aleatoire (nb_entiers-1) borne_max;;
 
 
 
@@ -130,3 +129,91 @@ let rec tri_pivot l =
   |h::t ->
       match partitionne_pivot l h with
       |(l1, l2, l3) -> tri_pivot l1 @ l2 @ tri_pivot l3;;
+
+
+
+
+
+
+
+
+
+(* ------------ *)
+(* TRI COMPTAGE *)
+(* ------------ *)
+
+(*
+compter_tous v l compte le nombre de fois où la valeur v apparaı̂t dans l.
+*)
+let rec compter_tous v l =
+  match l with
+  |[] -> 0
+  |h::t ->
+      if h = v
+      then 1+compter_tous v t
+      else compter_tous v t;;
+
+
+
+(*
+enlever_tous v l supprime toutes les occurences de v dans l.
+*)
+let rec enlever_tous v l =
+  match l with
+  |[] -> failwith "La liste est vide, aucun élément à supprimer."
+  |h::[] ->
+      if h = v
+      then []
+      else [h]
+  |h::t ->
+      if h = v
+      then enlever_tous v t
+      else h :: enlever_tous v t;;
+
+
+(*
+liste_associative l borne fin renvoie une liste associative, c’est-à-dire une liste de paires i,nb i,
+pour toutes les valeurs possibles de i dans l (i allant de 0 inclus à borne fin excluse).
+Chaque nb i correspond au nombre d’occurences de i dans l.
+*)
+let rec liste_associative l borne_fin = 
+  if borne_fin = 0
+  then failwith "Vous devez rentrer un nombre supérieur à 0"
+  else
+  if borne_fin = 1
+  then [(borne_fin-1, compter_tous (borne_fin-1) l)]
+  else
+    liste_associative (enlever_tous (borne_fin-1) l) (borne_fin-1) @ [(borne_fin-1, compter_tous (borne_fin-1) l)];;
+
+
+(*
+liste_entiers x nb x renvoie une liste de nb x fois l’entier x.
+*)
+let rec liste_entiers x nb_x = 
+  if nb_x = 0
+  then []
+  else [x] @ liste_entiers x (nb_x-1);;
+
+
+
+
+
+(*
+reconstituer_liste la reconstitue une liste classique,
+à partir d’une liste associative i,nb i (crée une liste avec nb i fois chaque i).
+*)
+let rec reconstituer_liste la = 
+  match la with 
+  |[] -> []
+  |(nb, i)::t -> liste_entiers nb i @ reconstituer_liste t;;
+
+
+
+(*
+tri_comptage l borne_max crée la liste associative correspondant à l et sa borne max,
+puis reconstitue la liste triée correspondante.
+*)
+let tri_comptage l borne_max =
+  match l with
+  |[] -> failwith "La liste est vide, aucun élément à trier"
+  |_ -> reconstituer_liste (liste_associative l borne_max);;
